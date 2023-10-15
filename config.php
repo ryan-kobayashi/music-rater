dde86a93371c3eaff4584a313cad096014c311e1
Haoran Xu <sxu2469@gmail.com>
2023-10-15 19:37:43 -0400

    config.php to establish connection between webpage and database

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php /* connection between webpage and database */
+
+$env = parse_ini_file('.env');
+
+$servername = $env["SERVER_NAME"];
+$dbname = $env["DATABASE_NAME"];
+$username = $env["USERNAME"];
+$password = $env["PASSWORD"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
