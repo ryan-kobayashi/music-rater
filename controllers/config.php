3be3c1d2472406fdce40e7b4d2fc76e002cfeabc
Haoran Xu <sxu2469@gmail.com>
2023-10-17 20:08:34 -0400

    moved config

---
+++ controllers/config.php
@@ -0,0 +1,19 @@
+<?php /* connection between webpage and database */
+
+$env = parse_ini_file('../.env');
+
+$servername = $env["SERVER_NAME"];
+$dbname = $env["DATABASE_NAME"];
+$username = $env["USERNAME"];
+$password = $env["PASSWORD"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+session_start();
+echo "Connected successfully";
+
+?>
