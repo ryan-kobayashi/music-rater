7489585ec2bdf1a98c2ea6e998a7da9515784aa7
ryan-kobayashi <rkobayashi@wesleyan.edu>
2023-10-18 00:27:35 -0400

    Implmented signup as well as redundantly check for incorrect input

--- signup.php
+++ signup.php
@@ -1,3 +1,73 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    
+
+    $invalidusername = false;
+    $takenusername = false;
+    $invalidpassword = false;
+    $invalidcpassword = false;
+
+# Only do this stuff in the case that the request method of the form is post.
+if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+    # Include connections to the database noted in config
+    include 'config.php';
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $cpassword = $_POST["cpassword"];
+    
+    # Validation that username is between 3 and 20 characters.
+    if (strlen($username) < 3 || strlen($username) > 20){
+        $invalidusername = true;
+    }
+
+    # Validation that password is between 10 and 25 characters.
+    if (strlen($password) < 10 || strlen($password) > 25){
+        $invalidpassword = true;
+    }
+
+    # Validation that the password matches the confirm password.
+    if ($password !== $cpassword) {
+        $invalidcpassword = true;
+    }
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+
+    $result = mysqli_query($conn, $sql);
+
+    $num = mysqli_num_rows($result);
+
+    # Validation that the username is not already taken
+    if ($num !== 0){
+        $takenusername = true;
+    }
+
+    if (!$takenusername && !$invalidcpassword && !$invalidpassword && !$invalidusername){
+        # This chunk of code prevents SQL Injection
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $stmt = $conn->stmt_init();
+        if ( ! $stmt->prepare($sql)){
+            die("SQL error: " . $conn->error);
+        }
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+
+        # This will start a session and bring you to read.php.
+        session_start();
+            
+        $_SESSION["username"] = $username;
+        $_SESSION["loggedin"] = true;
+        
+        # Here we want to add where the page redirects the user after logging in.
+        header("Location: controllers/read.php");
+        exit;
+    }
+    
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,21 +83,22 @@
 <body>
     <div class="container">
         <a href="index.html" class="back-button">Go back</a>
-        <form class="form" id="signup">
+        <form class="form" action="signup.php" method="post" id="signup">
             <h1 class="form__title">Sign Up</h1>
             <div class="form__message form__message--error"></div>
             <div class="form__input-group">
-                <input type="text" class="form__input" autofocus placeholder="Username" required
+                <label for="username"></label>
+                <input type="text" class="form__input" id="username" name="username" autofocus placeholder="Username" required
                 minlength="3" maxlength="20" >
                 <div class="form__input-error-message"></div>
             </div>
             <div class="form__input-group">
-                <input type="password" class="form__input" placeholder="Password" required
+                <input type="password" class="form__input" id="password" name="password" placeholder="Password" required
                 minlength="10" maxlength="25">
                 <div class="form__input-error-message"></div>
             </div>
             <div class="form__input-group">
-                <input type="password" class="form__input" placeholder="Confirm Password" required
+                <input type="password" class="form__input" id="cpassword" name="cpassword" placeholder="Confirm Password" required
                 minlength="10" maxlength="25">
                 <div class="form__input-error-message"></div>
             </div>
